ca0d838ebe91d4b7f73a667db65af0a5dad62001
Samay Samrat <samaysamrat@Samays-MacBook-Pro.local>
2026-01-04 08:18:02 +0530

    update

--- client/src/components/Testimonials.jsx
+++ client/src/components/Testimonials.jsx
@@ -4,7 +4,7 @@ import { delay, easeIn, inView, motion } from 'framer-motion'
 
 
 const testtis = [
-    {user: "Samay", image:'https://upload.wikimedia.org/wikipedia/commons/d/d0/Samay_raina.jpg', feedback : "This platform makes lesson planning faster and smarter. It helps me create engaging classes with AI-powered insights. "},
+    {user: "Samay", image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYoZOxbym-JFsvyG48OLldIKTwQRbU1hfvJA&s', feedback : "This platform makes lesson planning faster and smarter. It helps me create engaging classes with AI-powered insights. "},
     {user: "Samay", image:'https://media.licdn.com/dms/image/v2/D4E35AQFZxnX9oI2MzQ/profile-framedphoto-shrink_400_400/B4EZr82a4mKgAg-/0/1765178716036?e=1768096800&v=beta&t=u8hBT329_TWGg1js8LAwka5RH8973dGnEqOhI4MF4KI', feedback : "“With this platform, I spend less time planning and more time teaching. The AI suggestions really improve my lessons.” "},
     {user: "Ranbir", image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBFEhnaIH-7vxs9rm-7FM81uzztc5l1gpXzQ0b8Aa1LY6vk_O5YbQSEhS_PkrflJZ52Z4FrFXy4WRt39dBnaCl6wjDPr4wHVs6v7tcvg&s=10', feedback : "This platform is an essential tool for modern classrooms. Its AI guidance helps me design clear, structured, and interactive lesson plans "},
     {user: "Ashish", image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTokwl-nvPD6R-_HhQzjNGI-gbx1mHibrGYLIBarnY-q6v0iMbm0UnCag-0SY8G4m1elAvQ11-2r7UjsiOYSgE4hpdsxiOCfLVKX5-XA&s=10', feedback : "This platform completely changed how I plan my classes. The AI insights save hours and help me teach with more confidence. "},
